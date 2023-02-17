4b1155f7d6e3d4fe05c1e4e2d799e1e5300e5976
zanzipanziby <zanzipanziby@gmail.com>
2023-02-17 14:23:52 +0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState, KeyboardEvent, useRef} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [seconds, setSeconds] = useState(60)
+    const [isCounting, setIsCounting] = useState(false)
+    const [background, setBackground] = useState('white')
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            isCounting &&
+                setSeconds( seconds => seconds >= 1 ? seconds - 1 : 0)
+            }, 1000
+        )
+        return () => clearInterval(interval)
+    }, [isCounting])
+
+    const onPress = (e: KeyboardEvent<HTMLInputElement>) => {
+        debugger
+        if(e.key=== 'Enter' && isCounting){
+            setIsCounting(false)
+            setBackground('green')
+        }
+        if(e.key === ' ' && isCounting) {
+            isCounting &&
+            setIsCounting(false)
+            isCounting &&
+            setBackground('red')
+        }
+        if(e.key === 'Escape') {
+            setIsCounting(false)
+            setSeconds(60)
+            setBackground('white')
+        }
+    }
+
+    const onClickHandler = () => {
+        setSeconds(60)
+        setIsCounting(true)
+        setBackground('white')
+        // @ts-ignore
+        buttonRef.current.blur()
+
+    }
+
+
+
+
+    return (
+        <div className="App">
+
+            <div className={'wrapper'} onKeyDown={onPress} tabIndex={0} style={{background:background}}>
+                <div>
+                    <div className={'timer'}>{seconds}</div>
+                    <div style={{display:"flex", justifyContent: "center" }}>
+                        <div className={"startButton"}  onClick={onClickHandler}>START</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
